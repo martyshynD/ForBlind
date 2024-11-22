aa9145a8bd9a4e016a0aa9fc741b3c3f2e6eecb1
martyshynD <132333148+martyshynD@users.noreply.github.com>
2024-11-22 22:06:18 +0200

    Add files via upload

---
+++ sketch_oct25a.ino
@@ -0,0 +1,57 @@
+// * ---------------------    --------------------
+// * | HC-SC04 | Arduino |    | 3 pins | Arduino |
+// * ---------------------    --------------------
+// * |   Vcc   |   5V    |    |   Vcc  |   5V    |
+// * |   Trig  |   12    | OR |   SIG  |   13    |
+// * |   Echo  |   13    |    |   Gnd  |   GND   |
+// * |   Gnd   |   GND   |    --------------------
+// * ---------------------
+
+
+// driver - TB6612
+
+
+/// ultrasonic
+#include <Ultrasonic.h>
+Ultrasonic ultrasonic(12, 13);
+int distance;
+
+/// for driver
+int pwm = 7;
+int ain2 = 8;
+int ain1 = 9;
+float i;
+
+void setup() {
+  pinMode(pwm, OUTPUT); // pwm 
+  pinMode(ain2, OUTPUT); // AIN 2 
+  pinMode(ain1, OUTPUT); // AIN 1
+  Serial.begin(9600);
+}
+
+void loop() {
+  distance = ultrasonic.read();
+  
+  // Цикл, який виводить повідомлення "No!", коли відстань більше 30 см
+  while (distance > 50) {
+      Serial.println("Too far!");
+      delay(300);
+      digitalWrite(ain1, 0);
+      digitalWrite(ain2, 0);
+      distance = ultrasonic.read(); 
+  }
+
+  Serial.print("Distance in CM: ");
+  Serial.println(distance);
+//  delay(100);
+
+  // Обчислюємо i, щоб вона зменшувалася з відстанню
+  i = 255 - (distance * 5.1); // коригуємо коефіцієнт 8.5 за необхідності
+
+  // Обмежуємо значення i в межах 0-255
+  i = constrain(i, 0, 255);
+
+  analogWrite(pwm, i);
+  digitalWrite(ain2, LOW);
+  digitalWrite(ain1, HIGH);
+}
