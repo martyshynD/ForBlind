6f0ee3cbcd74f8c7bbbde5ae1f373c97304a2f49
martyshynD <132333148+martyshynD@users.noreply.github.com>
2025-03-06 19:47:52 +0000

    Update sketch_oct25a.ino

--- sketch_oct25a.ino
+++ sketch_oct25a.ino
@@ -3,20 +3,22 @@
 // Автор проєкту: Дмитро Мартишин
 // Помічник, науковий керівник: Андрій Мишопита
 
-// * ---------------------    --------------------
-// * | HC-SR04 | Arduino |    | 3 pins | Arduino |
-// * ---------------------    --------------------
-// * |   Vcc   |   5V    |    |   Vcc  |   5V    |
-// * |   Trig  |   12    | OR |   SIG  |   13    |
-// * |   Echo  |   13    |    |   Gnd  |   GND   |
-// * |   Gnd   |   GND   |    --------------------
-// * ---------------------
+#include <Arduino.h>
 
+// Кнопка
+#define PIN_BTN 9
+int status = 0;
 
-// Ультразвуковий датчик HC-SR04 
-#include <Ultrasonic.h>
-Ultrasonic ultrasonic(12, 13);
-int distance;
+// PIR датчик HC-SR501
+int led = 13; 
+int pin = 2;
+int pin_buz = 8; 
+bool state; 
+
+// Оптичний датчик VL53L1X
+#include <Wire.h>
+#include <VL53L1X.h>
+VL53L1X sensor;
 
 // Драйвер моторів TB6612FNG
 int pwm = 3;
@@ -26,35 +28,80 @@ int ain1 = 6;
 // значення поданої напруги на вібромоторчик (за принципом ШИМ) 
 float i;
 
+
+// Функція для оптичного датчику
+void opticfunc(){
+  noTone(pin_buz);
+  sensor.read();
+  if (sensor.ranging_data.range_mm >= 1200) {
+    analogWrite(pwm, 0);
+    digitalWrite(ain1, 0);
+    digitalWrite(ain2, 0);
+  }
+  else {
+    i = map(sensor.ranging_data.range_mm, 1200, 0, 0, 255);
+    analogWrite(pwm, i);
+    digitalWrite(ain2, LOW);
+    digitalWrite(ain1, HIGH);  
+  }
+}
+
+
+
+
+// Функція для PIR датчика
+void pirfunc(){
+  state = digitalRead(pin); // Використовуємо глобальну змінну
+  if (state == HIGH) { 
+    digitalWrite(led, HIGH);
+    tone(pin_buz, 880);
+  } else {
+    digitalWrite(led, LOW);
+    noTone(pin_buz);
+  }
+  analogWrite(pwm, LOW);
+  digitalWrite(ain2, LOW);
+  digitalWrite(ain1, LOW);    
+}
+
 void setup() {
+  Serial.begin(9600);
+  pinMode(PIN_BTN, INPUT_PULLUP);
+  pinMode(pin, INPUT);
+  pinMode(led, OUTPUT);
   pinMode(pwm, OUTPUT); // pwm 
   pinMode(ain2, OUTPUT); // AIN 2 
   pinMode(ain1, OUTPUT); // AIN 1
-  Serial.begin(9600);
+  Wire.begin();
+  Wire.setClock(400000); // use 400 kHz I2C
+  sensor.setTimeout(100);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1);
+  }
+  sensor.setDistanceMode(VL53L1X::Long);
+  sensor.setMeasurementTimingBudget(50000);
+  sensor.startContinuous(50);  
 }
 
 void loop() {
 
-  // зчитуємо значення з датчика відстані
-  distance = ultrasonic.read();
-  
-  // Цикл, який вимикає вібромотор, коли відстань більше 120 см
-  while (distance > 120) {
-      Serial.println("Too far!");
-      delay(300);
-      digitalWrite(ain1, 0);
-      digitalWrite(ain2, 0);
-      distance = ultrasonic.read(); 
+  if (digitalRead(PIN_BTN)== 0){
+    delay(200);
+    status++;
+  }
+  if (status > 2){
+    status = 1; 
   }
 
-  // Обчислюємо i - , щоб воно зменшувалася зі збільшенням відстані, та збільшувалось зі зменшенням відстані
-  i = 255 - (distance * 2.1); // коригуємо коефіцієнт 2.1 за необхідності (це потрібно для релюгування відстані)
+  if (status == 1){
+    Serial.println("Mode OPTIC");
+    opticfunc();
+  }
 
-  // Обмежуємо значення i в межах 0-255
-  i = constrain(i, 0, 255);
-
-  // подаємо струм на моторчик із певною напругою, маючи вже розраховане значення i (ШИМ)
-  analogWrite(pwm, i);
-  digitalWrite(ain2, LOW);
-  digitalWrite(ain1, HIGH);
+  if (status == 2){
+    Serial.println("Mode PIR");
+    pirfunc();
+  }
 }
